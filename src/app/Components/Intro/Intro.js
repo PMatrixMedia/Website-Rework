be63fd4c76fc1cc94e82b2e5321c8d003f1f81e6
PMatrixMedia <33465098+PMatrixMedia@users.noreply.github.com>
2026-02-15 19:50:19 +0100

    Replaced the heading for the intro with banner and cleaned up the alignment
    also added a font for the footer.

--- src/app/Components/Intro/Intro.js
+++ src/app/Components/Intro/Intro.js
@@ -1,30 +1,33 @@
 "use client";
 
 import React from "react";
-import { Theme , Box, Container, Heading, Flex, Text } from "@radix-ui/themes";
+import Image from "next/image";
+import { Theme, Box, Text } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import SphereScene from "./SphereScene";
 
 const Intro = () => {
   return (
     <Theme appearance="dark" accentColor="gray" grayColor="slate">
-      <Box size="3" align="center" justify="center">
-        <Heading size="9" weight="bold" className="text-white">
-            PhaseMatrix Media
-          </Heading>
+        <Box className="w-full max-w-[1400px] mx-auto px-4 flex flex-col items-center"align="center" justify="center">
+          <Box className="py-6 w-full">
+            <Image
+              src="/images/phasematrix-banner.png"
+              alt="PhaseMatrix Media"
+              width={1400}
+              height={321}
+              className="object-contain w-full"
+            />
           </Box>
-          <Box>
-          <Container>
+          <Box className="w-full">
             <SphereScene />
-          </Container>
           </Box>
-          <Box>
-          <Container size="3" align="center" justify="center">
-            <Text size="9" color="orange">
-                Click the Spheres to explore the site
-              </Text>
-            </Container>
+          <Box className="w-full flex justify-center py-6">
+            <Text size="9" color="orange" className="nabla-footer text-center">
+              Click the Spheres to explore the site.
+            </Text>
           </Box>
+        </Box>
     </Theme>
   );
 };
