50b1bb9a80609c78876b1dae6af0861677125c5f
PMatrixMedia <33465098+PMatrixMedia@users.noreply.github.com>
2026-02-15 22:07:54 +0100

    updated next and removed use client

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,11 @@
 import React from "react";
 import About from "../Pages/About/about";
 
+export const metadata = {
+  title: "About Me",
+  description: "Learn more about me and my portfolio.",
+};
+
 export default function AboutPage() {
   return <About />;
 }
